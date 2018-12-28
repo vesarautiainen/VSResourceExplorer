399f23f91b71bf933a511856708e409fa519e841
Vesa Rautiainen <verautia@microsoft.com>
2018-12-28 15:29:53 +0200

    Move files around and rename them. Add some dummy content for the Custom XML feature. At least a dialog is shown now

--- VSResourceExplorer/Pages/CustomXamlPage.xaml.cs
+++ VSResourceExplorer/Pages/CustomXamlPage.xaml.cs
@@ -24,5 +24,27 @@ namespace VSResourceExplorer
         {
             InitializeComponent();
         }
+
+        private void ShowButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var dialog = new BaseDialogWindow();
+            dialog.Title = "This is the custom dialog";
+
+            var button = new Button();
+            button.Content = "Test Button";
+            button.Width = 95;
+            button.Height = 35;
+
+            var stackPanel = new StackPanel();
+            stackPanel.Children.Add(button);
+
+            dialog.Content = stackPanel;
+            dialog.ShowDialog();
+        }
+
+        private void TxtXaml_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
