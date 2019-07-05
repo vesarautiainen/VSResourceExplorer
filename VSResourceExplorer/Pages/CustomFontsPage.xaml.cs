7ceae2789ca325502187ae090f4e857ef3e889de
Vesa Rautiainen <verautia@microsoft.com>
2019-07-05 10:10:28 +0300

    Updated regex for font text box and updated vsix.

--- VSResourceExplorer/Pages/CustomFontsPage.xaml.cs
+++ VSResourceExplorer/Pages/CustomFontsPage.xaml.cs
@@ -28,8 +28,8 @@ namespace VSResourceExplorer
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            //Regex regex = new Regex("[^0-9.]+");
-            Regex regex = new Regex("^[0-9]*\\.[0-9][0-9]$");
+            Regex regex = new Regex("[^0-9.]+");
+           
             e.Handled = regex.IsMatch(e.Text);
         }
     }
