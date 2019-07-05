528a2572f880d7e780c138056ed5e7c5d2c4701f
Vesa Rautiainen <verautia@microsoft.com>
2019-07-05 09:58:44 +0300

    Hide font size slider and have text box instead. Change the regex validation (seems to be broken still) Update vsix.

--- VSResourceExplorer/Pages/CustomFontsPage.xaml.cs
+++ VSResourceExplorer/Pages/CustomFontsPage.xaml.cs
@@ -29,7 +29,7 @@ namespace VSResourceExplorer
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             //Regex regex = new Regex("[^0-9.]+");
-            Regex regex = new Regex("^\\d{ 1,3} (\\.\\d{ 1,2})?$");
+            Regex regex = new Regex("^[0-9]*\\.[0-9][0-9]$");
             e.Handled = regex.IsMatch(e.Text);
         }
     }
